075b3d965ecaa4ded5ea34b260cf96948719da5a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-22 10:47:48 +0600

    Explored string initialization

---
+++ StringInitialization.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // char stringArray[6] = {'A', 'S', 'H', 'I', 'K'};
+    char stringArray[6] = "ASHIK";
+
+    printf("%s", stringArray);
+
+    return 0;
+}
